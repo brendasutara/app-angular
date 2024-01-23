d76f519750763cbaea3a995fd428ab89eda50e89
brendasutara <sutarabrenda@gmail.com>
2024-01-23 20:45:41 -0300

    Event binding: click, doble click y keydown

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
+  //esto seria propiedades de esta clase
   welcome = 'Bienvenido a mi primera aplicación con Angular';
   tasks = [
     'Instalar Angular CLI',
@@ -18,5 +20,25 @@ export class LabsComponent {
   name = 'Brenda';
   age = 27;
   disabled = true;
-  img = 'https://cdn.pixabay.com/photo/2021/10/05/17/24/owl-figurine-6683338_640.jpg'
+  img = 'https://cdn.pixabay.com/photo/2021/10/05/17/24/owl-figurine-6683338_640.jpg';
+
+  person = {
+    name: 'Cata',
+    age: 27,
+    avatar: 'https://cdn.pixabay.com/photo/2018/02/21/08/40/woman-3169726_1280.jpg'
+  }
+
+  //metodo de la clase, una funcion dentro de este componente
+  clickHandler() {
+    alert('Hola ' + this.person.name + '! vos tenés ' + this.person.age + ' años')
+  }
+
+  changeHandler(event: Event) {
+    console.log();
+  }
+
+  keydownHanler (event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 }
