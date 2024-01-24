af9d9107f5fda44fa7944e05194fff6f039f561f
brendasutara <sutarabrenda@gmail.com>
2024-01-24 00:02:37 -0300

    tasks como objetos

--- src/app/pages/homes/homes.component.ts
+++ src/app/pages/homes/homes.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task } from '../../models/task.model';
 
 @Component({
   selector: 'app-homes',
@@ -9,18 +10,47 @@ import { CommonModule } from '@angular/common';
   styleUrl: './homes.component.css'
 })
 export class HomesComponent {
-  tasks = signal ([
-    'Instalar Angular CLI',
-    'Crear proyecto',
-    'Crear componenetes',
-    'Crear servicio',
+  tasks = signal <Task[]> ([
+    {
+      id: Date.now(),
+      title: 'Instalar Angular CLI',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear proyecto',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear componenetes',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear servicio',
+      completed: false
+    },
+
+
+
+
   ]);
 
   changeHandler (event: Event) {
     const input = event.target as HTMLInputElement;
     const newTask = input.value;
-    //update va a crear un nuevo estado, lo agrega
-    this.tasks.update((tasks)=> [...tasks, newTask])
+    this.addTask(newTask);
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+     //update va a crear un nuevo estado, lo agrega
+     this.tasks.update((tasks)=> [...tasks, newTask])
   }
 
   deleteTask(index: number) {
